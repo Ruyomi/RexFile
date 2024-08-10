8416b5aa41a92cb9ee4015a60e80f7d44d22e71d
MingYubmy <1905065952@qq.com>
2024-08-11 00:14:48 +0800

    优化Root逻辑，加入复用逻辑

--- RexFile/src/main/kotlin/com/ruyomi/dev/utils/rex/file/utils/RootUtil.kt
+++ RexFile/src/main/kotlin/com/ruyomi/dev/utils/rex/file/utils/RootUtil.kt
@@ -1,58 +1,75 @@
 package com.ruyomi.dev.utils.rex.file.utils
 
+import java.io.BufferedReader
 import java.io.DataOutputStream
 import java.io.InputStreamReader
+import java.util.concurrent.locks.ReentrantLock
 
 internal object RootUtil {
 
-    fun hasPermission(): Boolean {
-        return try {
-            Runtime.getRuntime().exec("su -c exit").waitFor() == 0
-        } catch (_: Exception) {
-            false
+    private var process: Process? = null
+    private var outputStream: DataOutputStream? = null
+    private var inputStream: BufferedReader? = null
+
+    private val lock = ReentrantLock()
+
+    private fun newProcess(useRoot: Boolean = true) = try {
+        lock.lockInterruptibly()
+        process = Runtime.getRuntime().exec(if (useRoot) "su" else "sh")
+        outputStream = DataOutputStream(process?.outputStream)
+        inputStream = BufferedReader(InputStreamReader(process?.inputStream))
+    } catch (_: Exception) {
+    } finally {
+        lock.unlock()
+    }
+
+    fun exit() = try {
+        outputStream?.close()
+        inputStream?.close()
+        process?.destroy()
+    } catch (_: Exception) {
+    }
+
+    fun hasPermission() = try {
+        Runtime.getRuntime().exec("su -c exit").waitFor() == 0
+    } catch (_: Exception) {
+        false
+    }
+
+    fun requestPermission() = executeCommand("").first == 0
+
+    fun executeCommand(command: String, useRoot: Boolean = true) = try {
+        if (process == null) newProcess(useRoot)
+
+        lock.lockInterruptibly()
+
+        outputStream?.apply {
+            writeBytes(command)
+            writeBytes("\necho \"|<<exitCode:\$?|\"")
+            writeBytes("\necho \"|<<exitWait|\"\n")
+            flush()
         }
-    }
 
-    fun requestPermission(): Boolean {
-        return executeCommand("").first == 0
-    }
-
-    fun executeCommand(command: String, useRoot: Boolean = true): Pair<Int, String> {
-        return try {
-            val process = Runtime.getRuntime().exec(if (useRoot) "su" else "sh")
-
-            val outStream = process.outputStream
-            val inStream = process.inputStream
-            val errStream = process.errorStream
-
-            val outputStream = DataOutputStream(outStream)
-            val inputStream = InputStreamReader(inStream)
-            val errorStream = InputStreamReader(errStream)
-
-            outputStream.apply {
-                writeBytes("$command\n")
-                flush()
-                writeBytes("exit\n")
-                flush()
+        val output = StringBuilder()
+        var exitCode = -1
+        while (true) {
+            val line = inputStream?.readLine()
+            if (line != null && line.startsWith("|<<exitCode:")) {
+                exitCode = line.run {
+                    substring(
+                        startIndex = "exitCode:".let { indexOf(it) + it.length },
+                        endIndex = lastIndexOf("|")
+                    )
+                }.toIntOrNull() ?: -1
             }
-
-            val output = StringBuilder()
-            inputStream.forEachLine { output.append(it).append("\n") }
-            errorStream.forEachLine { output.append(it).append("\n") }
-
-            val exitValue = process.waitFor()
-
-            errorStream.close()
-            inputStream.close()
-            outputStream.close()
-
-            errStream.close()
-            inStream.close()
-            outStream.close()
-
-            Pair(exitValue, output.toString())
-        } catch (e: Exception) {
-            Pair(-1, e.toString())
+            if (line == null || line == "|<<exitWait|") break
+            output.append(line).append("\n")
         }
+
+        Pair(exitCode, output.toString())
+    } catch (e: Exception) {
+        Pair(-1, e.toString())
+    } finally {
+        lock.unlock()
     }
 }
